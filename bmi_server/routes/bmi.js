bb4d467ad5ec804bafc2d05457c982af65924ae1
MG1382JS <mahdiJs1382@gmail.com>
2024-01-11 10:15:52 +0330

    compelte server project

--- bmi_server/routes/bmi.js
+++ bmi_server/routes/bmi.js
@@ -4,22 +4,29 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
 
+
 router.get('/', async function (req, res, next) {
     let db = req.body;
     let cmConvertToMeter = db.height / 100
     let calculateBmi = db.weight / Math.pow(cmConvertToMeter, 2)
    
     db.ip = req.ip
-
     const createdchat = await prisma.userBmi.create({
         data: db,
     })
 
-    res.send(createdchat);
-
-
-
+    res.send(createdchat +'  '+calculateBmi);
 
 });
+router.get('/show',async function(req,res,next){
+    const showBmi = await prisma.userBmi.findMany({
+        orderBy: {
+            id: 'desc',
+          },
+    });
+
+    res.send(showBmi)
+
+})
 
 module.exports = router;
